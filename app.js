1260dd62d68da722b4a9746fa056cbf9477142b6
Nat <nat.wrw@gmail.com>
2020-08-03 22:15:40 +0700

    no message

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const fileUpload = require("express-fileupload");
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const fileUpload = require('express-fileupload')
 // var http = require("http");
-const _ = require("lodash");
+const _ = require('lodash')
 
-var cors = require("cors");
-var app = express();
+var cors = require('cors')
+var app = express()
 
-app.use(cors());
+app.use(cors())
 // enable files upload
-app.use(fileUpload({
-  createParentPath: true,
-  limits: {
-    fileSize: 2 * 1024 * 1024 * 1024 //2MB max file(s) size
-  }
-}));
+app.use(
+  fileUpload({
+    createParentPath: true,
+    limits: {
+      fileSize: 2 * 1024 * 1024 * 1024, //2MB max file(s) size
+    },
+  })
+)
 
 //add other middleware
-app.use(express.static("public"));
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("dev"));
+app.use(express.static('public'))
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(morgan('dev'))
 
 //start app
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-app.listen(port, () =>
-  console.log(`App is listening on port ${port}.`)
-);
+app.listen(port, () => console.log(`App is listening on port ${port}.`))
